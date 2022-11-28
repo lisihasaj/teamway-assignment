9e4d5202db0ef2bbb6762dc2884f402a25f0fb29
Lisjan Hasaj <lisjan@kutia.net>
2022-11-28 22:38:05 +0100

    Questions views added and unnecessary vendors uninstalled

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,10 @@
+import { useNavigate } from "react-router-dom";
+import { PATHS } from "@routes/paths";
 import Button from "@components/ui/Button";
 
 export default function Home() {
+    const push = useNavigate();
+
     return (
         <div className="w-full flex flex-col items-center">
             <div className="w-full flex flex-col items-center text-center mt-4 mb-6">
@@ -12,7 +16,7 @@ export default function Home() {
                     and get your results in the end.
                 </span>
             </div>
-            <Button arrow className="my-6">
+            <Button arrow className="my-6" onClick={() => push(PATHS.survey)}>
                 Start the survey
             </Button>
         </div>
--- src/routes/paths.ts
+++ src/routes/paths.ts
@@ -1,15 +1,6 @@
-interface Paths {
-    home: string;
-    test: string;
-    finishScreen: string;
-    notFound: string;
-}
-
-export const PATHS = (slug: string = ""): Paths => {
-    return {
-        home: `/`,
-        test: `/test/${slug}`,
-        finishScreen: `/test-completed`,
-        notFound: `*`,
-    };
+export const PATHS = {
+    home: `/`,
+    survey: `/survey`,
+    completedSurvey: `/survey-completed`,
+    notFound: `*`,
 };
